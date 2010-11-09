bb4f417b343cd69c6f346e2083714775346442ec
fernanda.dq <fernanda.dq@5851dbaf-38c6-0ca8-c318-f9485879d080>
2010-11-09 00:06:51 +0000

    [TESTE]

--- trunk/RestaurantePOO/src/restaurantepoo/Main.java
+++ trunk/RestaurantePOO/src/restaurantepoo/Main.java
@@ -15,7 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String teste;
+        String test;
+        
     }
 
 }

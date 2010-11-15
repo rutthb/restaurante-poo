17c389a1d4d34f9a2aa54eb339ffe3920cf146d4
bootstrapper.br <bootstrapper.br@5851dbaf-38c6-0ca8-c318-f9485879d080>
2010-11-15 12:33:35 +0000

    Implementados os atributos, gets e sets e construtores do pacote lógica em todas as classes

--- RestaurantePOO/src/restaurantepoo/dao/ConfiguracaoDao.java
+++ RestaurantePOO/src/restaurantepoo/dao/ConfiguracaoDao.java
@@ -5,10 +5,25 @@
 
 package restaurantepoo.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import restaurantepoo.bancodados.CriaConexao;
+import restaurantepoo.logica.Configuracao;
+
 /**
  *
  * @author Antonio
  */
 public class ConfiguracaoDao {
+
+    private Connection conexao;
+
+    public ConfiguracaoDao() throws SQLException{
+        this.conexao = CriaConexao.getConexao();
+    }
 
 }
--- RestaurantePOO/src/restaurantepoo/logica/CupomFiscal.java
+++ RestaurantePOO/src/restaurantepoo/logica/CupomFiscal.java
@@ -11,4 +11,43 @@ package restaurantepoo.logica;
  */
 public class CupomFiscal {
 
+    private int cupomFIscal;
+    private enum tipoPagamento {dinheiro,credito,debito};
+    private double valorPago;
+    private String cpfCliente;
+
+    public CupomFiscal() {
+    }
+
+    public CupomFiscal(double valorPago, String cpfCliente) {
+        this.valorPago = valorPago;
+        this.cpfCliente = cpfCliente;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public int getCupomFIscal() {
+        return cupomFIscal;
+    }
+
+    public void setCupomFIscal(int cupomFIscal) {
+        this.cupomFIscal = cupomFIscal;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+
+
 }

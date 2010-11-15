41ab6a8ab3fecfa9bb888cb0e28392d40fbd58e1
bootstrapper.br <bootstrapper.br@5851dbaf-38c6-0ca8-c318-f9485879d080>
2010-11-15 13:06:55 +0000

    Implementados os métodos da classe ProdutoDao

--- RestaurantePOO/src/restaurantepoo/dao/ProdutoDao.java
+++ RestaurantePOO/src/restaurantepoo/dao/ProdutoDao.java
@@ -6,8 +6,13 @@
 package restaurantepoo.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import restaurantepoo.bancodados.CriaConexao;
+import restaurantepoo.logica.Produto;
 
 /**
  *
@@ -21,6 +26,71 @@ public class ProdutoDao {
         this.conexao = CriaConexao.getConexao();
     }
 
-    
+    public void adiciona(Produto p) throws SQLException{
+        String  sql = "insert into produto (nome, preco, descricao)" +
+                "values (?,?,?)";
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+
+        // Seta os valores
+        stmt.setString(1, p.getNome());
+        stmt.setString(2, String.valueOf(p.getPreco()));
+        stmt.setString(3, p.getDescricao());
+
+        // Executa o código SQL
+        stmt.execute();
+        stmt.close();
+    }
+
+    public  List<Produto> getLista(String busca) throws SQLException{
+        String sql = "Select * from produto where nome like ?";
+        PreparedStatement stmt = this.conexao.prepareStatement(sql);
+
+        stmt.setString(1, busca);       // inserção do caracter de busca.
+
+        ResultSet rs = stmt.executeQuery();
+
+        List<Produto> minhaLista = new ArrayList<Produto>();
+
+        while(rs.next()){
+            Produto p1 = new Produto();
+            p1.setProduto(rs.getInt("produto"));
+            p1.setNome(rs.getString("nome"));
+            p1.setPreco(Double.parseDouble(rs.getString("preco")));
+            p1.setDescricao(rs.getString("descricao"));
+
+            minhaLista.add(p1);
+        }
+
+        rs.close();
+        stmt.close();
+        return minhaLista;
+    }
+
+    public void altera(Produto p1) throws SQLException{
+        String sql = "update produto set nome=?, preco=?" +
+                        ", descricao=? where produto=?";
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+
+        // Seta os valores
+
+        stmt.setString(1, p1.getNome());
+        stmt.setString(2, String.valueOf(p1.getPreco()));
+        stmt.setString(3, p1.getDescricao());
+        stmt.setString(4, String.valueOf(p1.getProduto()));
+
+        // Executa o código SQL
+        stmt.execute();
+        stmt.close();
+    }
+
+    public void remove(Produto p1) throws SQLException{
+        String sql = "delete from produto where id=?";
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+
+        stmt.setString(1, String.valueOf(p1.getProduto()));
+
+        stmt.execute();
+        stmt.close();
+    }
 
 }

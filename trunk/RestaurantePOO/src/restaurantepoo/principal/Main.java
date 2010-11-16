9df2c053bf9fb13193c868f8ea75d214cdb4dd1f
fernanda.dq <fernanda.dq@5851dbaf-38c6-0ca8-c318-f9485879d080>
2010-11-16 01:36:26 +0000

    JFrame para Cad de Funcionario e Figuras

--- trunk/RestaurantePOO/src/restaurantepoo/principal/Main.java
+++ trunk/RestaurantePOO/src/restaurantepoo/principal/Main.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import restaurantepoo.bancodados.CriaConexao;
 import restaurantepoo.dao.FuncionarioDao;
 import restaurantepoo.dao.ProdutoDao;
+import restaurantepoo.forms.jFCadFuncionario;
 import restaurantepoo.logica.Funcionario;
 import restaurantepoo.logica.Produto;
 
@@ -22,15 +23,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException{
-
-        Produto p = new Produto("Sudo de Laranja", 2.50, "Suco de laranja natural 500 ml");
-        System.out.println(p.toString());
-
-        ProdutoDao pd = new ProdutoDao();
-        System.out.println("conexão realizada com sucesso");
-
-        pd.adiciona(p);
-
-    }
+    public static void main(String[] args){
+       new jFCadFuncionario().setVisible(true);
+           }
 }

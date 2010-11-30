c501e1c4603b6166f0b30fff2a2ac153dc58365d
fernanda.dq <fernanda.dq@5851dbaf-38c6-0ca8-c318-f9485879d080>
2010-11-30 03:25:11 +0000

    Formulário de Configuração

--- trunk/RestaurantePOO/src/restaurantepoo/dao/ConfiguracaoDao.java
+++ trunk/RestaurantePOO/src/restaurantepoo/dao/ConfiguracaoDao.java
@@ -7,6 +7,7 @@ package restaurantepoo.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import restaurantepoo.bancodados.CriaConexao;
 import restaurantepoo.logica.Configuracao;
@@ -29,7 +30,25 @@ public class ConfiguracaoDao {
         cd.adiciona(c);
     }
 
- public void adiciona(Configuracao c1) throws SQLException{
+     public void busca (Configuracao c1) throws SQLException{
+        String sql = "Select * from configuracao";
+        PreparedStatement stmt = this.conexao.prepareStatement(sql);
+
+        ResultSet rs = stmt.executeQuery();
+        while(rs.next()){
+            c1.setCnpj(rs.getString("cnpj"));
+            c1.setRazaoSocial(rs.getString("razaosocial"));
+            c1.setInscEstadual(rs.getString("inscestadual"));
+            c1.setNome(rs.getString("nome"));
+            c1.setEndereco(rs.getString("endereco"));
+            c1.setTelefone(rs.getString("telefone"));
+            c1.setNumeroMesas(rs.getInt("nummesas"));
+            c1.setDescricaoRefeicao(rs.getString("descricaorefeicao"));
+         }
+        }
+
+
+    public void adiciona(Configuracao c1) throws SQLException{
 
         String  sql = "insert into configuracao (cnpj, razaosocial, inscestadual, nummesas,  " +
                 "nome, endereco, telefone, descricaorefeicao) values (?,?,?,?,?,?,?,?)";
@@ -53,7 +72,7 @@ public class ConfiguracaoDao {
     }
 
     public void altera(Configuracao c1) throws SQLException{
-        String sql = "update configuracao set cnpj=?, razaosocial=?, inscestadual=?, mummesas=?, " +
+        String sql = "update configuracao set cnpj=?, razaosocial=?, inscestadual=?, nummesas=?, " +
                         "nome=?, endereco=?, telefone=?, descricaorefeicao=?";
 
         PreparedStatement stmt = conexao.prepareStatement(sql);

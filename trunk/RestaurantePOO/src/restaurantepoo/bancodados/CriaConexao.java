3d8f14f263c3a06d29ffa83be517caa8fae2c448
fernanda.dq <fernanda.dq@5851dbaf-38c6-0ca8-c318-f9485879d080>
2010-12-01 16:48:55 +0000

    Comentários

--- trunk/RestaurantePOO/src/restaurantepoo/bancodados/CriaConexao.java
+++ trunk/RestaurantePOO/src/restaurantepoo/bancodados/CriaConexao.java
@@ -1,27 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package restaurantepoo.bancodados;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 /**
+ * Registra o driver e estabelece uma conexão com o banco de dados MySQL.
+ * Captura de exceções SQLException é de uso obrigatório para JDBC.
+ * Informa o erro em caso de exceção.
  *
- * @author Antonio
  */
 public class CriaConexao {
-
+    //Registra o driver e estabelece uma conexão. Informa o erro em caso de exceção./
     public static Connection getConexao() throws SQLException {
-        try {
+        try { //Captura de exceções SQLException, uso obrigatório para JDBC
+            //Registra um driver
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Conectando ao Banco");
-            return DriverManager.getConnection("jdbc:mysql://localhost/restaurantepoo", "root", "admin");
+            //Estabelece uma conexão
+            return DriverManager.getConnection("jdbc:mysql://localhost/restaurantepoo", "root", "root");
 
         } catch (ClassNotFoundException e) {
+            //Exceção gerada para informar o erro
             throw new SQLException(e.getMessage());
         }
     }
